eac6e38f9e7f57b1b0a2e8426150d368ec573ea6
Ryan Le <31494954+lryanle@users.noreply.github.com>
2024-06-10 15:20:55 -0700

    fix crash

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,22 @@
 import CountdownTimer from "@/components/CountdownTimer"
 import { useState } from "react";
 
+type Team = {
+  name: string;
+  number: number;
+};
+
 export default function Home() {
-  const teams = [
+  const [selectedTeam, setSelectedTeam] = useState<Team>();
+
+  const teams: Team[] = [
     { name: 'Team Alpha', number: 1 },
     { name: 'Team Bravo', number: 2 },
     { name: 'Team Charlie', number: 3 },
     { name: 'Team Delta', number: 4 },
     { name: 'Team Echo', number: 5 },
   ];
-  
-  const [selectedTeam, setSelectedTeam] = useState();
+
 
   return (
     <div className="w-screen h-screen flex justify-center items-center flex-col gap-3">
@@ -21,7 +27,7 @@ export default function Home() {
           <p className="font-bold text-3xl">Select a team:</p>
           <ul className="flex flex-col justify-center items-center gap-1 w-full">
             {teams.map((team) => (
-              <li key={team.number} className="text-2xl cursor-pointer m-1 bg-white bg-opacity-10 px-3 py-1 rounded" onClick={() => setSelectedTeam(team)}>
+              <li key={team.number} className="text-2xl cursor-pointer m-1 bg-white bg-opacity-10 px-3 py-1 rounded" onClick={() => {setSelectedTeam(team)}}>
                 {team.name} ({team.number})
               </li>
             ))}
